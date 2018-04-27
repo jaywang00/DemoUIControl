c376294bbcb2da635c4360a914e5267f1414a6b6
Jay Wang <ielcw00@gmail.com>
2018-04-27 14:27:32 +0800

    v1.0

---
+++ app/src/main/java/com/example/demouicontrol/demouicontrol/EditTextEX.java
@@ -0,0 +1,13 @@
+package com.example.demouicontrol.demouicontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditTextEX extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edittext_ex);
+    }
+}
